0f85cd4780b2ce2224994a4014603f0bb6fcdebc
Constantin van Ypersele <constantin.vanypersele@student.uclouvain.be>
2021-11-02 12:51:27 +0100

    web-scrapping

---
+++ Hour.py
@@ -0,0 +1,143 @@
+"""
+Créé le 13/08/2021
+
+@Auteur : Constantin van Ypersele
+
+"""
+
+class Hour :
+    
+    def __init__(self, date, sunshine, wind, rain, temperature):
+        """
+        @date : Datetime object
+        @sunshine : int between 0 and 6
+        @wind : int
+        @rain : float ([mm])
+        @temperature : float (°C)
+        
+        """
+        self.date = date
+        self.sunshine = sunshine
+        self.wind = wind
+        self.rain = rain
+        self.temperature = temperature
+    
+    def get_date(self):
+        return self.date
+    
+    def get_sunshine(self):
+        return self.sunshine
+    
+    def get_wind(self):
+        return self.wind
+    
+    def get_rain(self):
+        return self.rain
+    
+    def get_temperature(self):
+        return self.temperature
+    
+    def set_date(self, new_date):
+        self.date = new_date
+        
+    def set_sunshine(self, new_sunshine):
+        self.sunshine = new_sunshine
+    
+    def set_wind(self, new_wind):
+        self.wind = new_wind
+    
+    def set_rain(self, new_rain):
+        self.rain = new_rain
+        
+    def set_temperature(self, new_temperature):
+        self.temperature = new_temperature
+        
+    def meteo_comparator(self, other_hour):
+        """
+        @other_hour : object of type Hour
+        
+        @return : -1 if self < other_hour, 1 if other_hour < self
+
+        """
+        if self.sunshine < other_hour.get_sunshine():
+            return 1
+        
+        first_cond =  (self.sunshine == other_hour.get_sunshine())
+        
+        if (first_cond) and (self.rain > other_hour.get_rain()):
+            return 1
+            
+        second_cond = first_cond and (self.rain == other_hour.get_rain())
+        if (second_cond) and (self.wind > other_hour.get_wind()):
+            return 1
+        
+        third_cond = second_cond and (self.wind == other_hour.get_wind())
+        if third_cond and self.date > other_hour.get_date():
+            return 1
+        
+        return -1
+    
+    def date_comparator(self, other_hour):
+        if self.date < other_hour.get_date():
+            return -1
+        
+        return 1
+    
+    def merge(list_a, list_b, comparator):
+        result = []
+        
+        index_a = 0
+        index_b = 0
+        
+        while index_a < len(list_a) and index_b < len(list_b) :
+            
+            if comparator(list_a[index_a], list_b[index_b]) == -1:
+                result.append(list_a[index_a])
+                index_a += 1
+                
+            else:
+                result.append(list_b[index_b])
+                index_b += 1
+        
+        while index_a < len(list_a) :
+            result.append(list_a[index_a])
+            index_a += 1
+        
+        while index_b < len(list_b) :
+            result.append(list_b[index_b])
+            index_b += 1
+        
+        return result
+    
+    def sort_list(hours, comparator):
+        """
+        @hours : list of Hour to be sorted
+        @return : new sorted list
+        
+        """
+        size = len(hours)   
+        if size == 1 :
+            return hours
+        
+        first = Hour.sort_list(hours[:size//2], comparator)
+        second = Hour.sort_list(hours[size//2:], comparator)
+
+        return Hour.merge(first, second, comparator)
+    
+    def __str__(self):
+        #message = "On {0} the sunshine will be {1}/6, there will be a wind of {2}".format(self.date, self.sunshine, self.wind)
+        #message += "km/h, {0} mm of rain and a temperature of {1} °C.".format(self.rain, self.temperature)
+        
+        message = "{0},      {1},        {2},       {3},         {4}\n".format(self.date, self.sunshine, self.wind, self.rain, self.temperature)
+        return message
+
+"""
+from datetime import datetime
+a = datetime(1920, 8, 15, 13)
+b = datetime(1920, 8, 15, 11)
+l = [Hour("12/5/2021", 5, 15, 0.2, 25.0), Hour("17/03/1599", 0, 0, 0, 3000), Hour("12/90/6600", 10, 10000, 2002, 5),
+     Hour(a, 15, 20, 1, 18), Hour(b, 15, 20, 1, -158), Hour(a, 3, 10, 10, 7), Hour(a, 3, 8, 177, 0)]
+
+for i in Hour.sort_list(l, Hour.meteo_comparator):
+   print(i)
+"""
---
+++ web_scrapping.py
@@ -0,0 +1,87 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+from Hour import *
+import requests
+import csv
+
+
+#url = "https://www.buienradar.be/weer/Louvain-la-Neuve/BE/2792073"
+url = "https://www.meteobelgique.be/previsions-meteo-belgique/heure-par-heure/1348/louvain-la-neuve"
+
+
+dic_nuage = {"sun.png" : 6, "lightcloudy.png" : 5, "lightsun.png" : 4, "cloudyshower.png" : 3, "cloudy.png" : 3, 
+             "mostcloudy.png" : 2, "cover.png" : 1, "lightrain.png" : 1, "lightshower.png" : 2, "rain.png" : 1,
+             "thundershower.png" : 1, "shower.png" : 1, "thunderrain.png" : 1, "moon" : 0}
+
+dic_date = {"janvier" : 1, "février" : 2, "mars" : 3, "avril" : 4, "mai" : 5, "juin" : 6, "juillet" : 7, "août" : 8,
+            "septembre" : 9, "octobre" : 10, "novembre" : 11, "décembre" : 12}
+
+def get_data(url):
+    """
+    @pre: url est le lien vers un site météo
+    @post retourne une liste d'objet Hour(@date, @ensoleimment, @vent, @precipitation, @temperature)
+    """
+    
+    temperature = 0
+    vent = 0 
+    precipitation = 0
+    hours = []
+
+    content = requests.get(url).content
+    soup = BeautifulSoup(content, features="lxml")
+    data = soup.findAll('tr')
+    size = len(data)-1
+    
+    year = datetime.now().year
+    
+    for i in range(1,size+1):
+        
+        word = data[i].text.split()
+        if word == [] :
+            size-=1
+            continue
+        
+        info = word[3].split(":00")
+
+        jour = datetime(year, dic_date[str.lower(word[2])], int(word[1]), int(info[0]))        
+    
+        info = info[1].split("°C")
+        temperature = float(info[0])
+    
+        if len(word) == 6:  #No rain
+            vent = int(info[1])
+            precipitation = 0.0
+        
+        elif len(word) == 7: #Rain [mm]
+            vent = int(word[4][2:])
+            precipitation = float(info[1])
+
+        else:
+            print("ERREUR : taille inconnue")
+            return
+    
+    
+        pic = str(data[i].find('img')).split()  #1e image is the sunshine
+        pic = pic[2].strip('"').split("/")
+        if pic[-2] == "night":
+            pic[-1] = "moon"
+        pic = pic[-1]
+        
+        hours.append(Hour(jour, dic_nuage[pic], vent, precipitation, temperature))
+        
+    return hours
+
+
+data = get_data(url)
+
+"""
+with open("data.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Date', 'Sunshine', 'Wind', 'Rain', 'Temperature'])
+    for i in data:
+        writer.writerow(str(i))
+"""
+with open("data.txt", 'w') as file:
+    file.write("        Date          Sunshine     wind      rain      temperature\n")
+    for i in data:
+        file.write(str(i))
