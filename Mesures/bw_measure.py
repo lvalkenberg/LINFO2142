72346f05a4c31241921fab6361829b9b66ec4f2f
lvalkenberg <louis.valkenberg@student.uclouvain.be>
2021-12-04 10:12:44 +0100

    struct repo

--- Mesures/bw_measure.py
+++ Mesures/bw_measure.py
@@ -13,7 +13,7 @@ import random
     return exemple :
         {'download': 45203916.74099848, 'upload': 0, 'ping': 41.488, 'server': {'url': 'http://ookla2.ictvanloon.nl:8080/speedtest/upload.php', 'lat': '51.4381', 'lon': '5.4752', 'name': 'Eindhoven', 'country': 'Netherlands', 'cc': 'NL', 'sponsor': 'ICTvanLoon', 'id': '45519', 'host': 'ookla2.ictvanloon.nl:8080', 'd': 107.95631998641474, 'latency': 41.488}, 'timestamp': '2021-11-11T09:35:42.333814Z', 'bytes_sent': 0, 'bytes_received': 56774196, 'share': None, 'client': {'ip': '81.246.166.119', 'lat': '50.6361', 'lon': '4.605', 'isp': 'Proximus', 'isprating': '3.7', 'rating': '0', 'ispdlavg': '0', 'ispulavg': '0', 'loggedin': '0', 'country': 'BE'}}
 """    
-def bw_speedtest():
+def bw_speedtest(t):
     # utility
     def get_lower_key(dic):
         min_key = None
@@ -23,17 +23,18 @@ def bw_speedtest():
         return min_key
 
     s = speedtest.Speedtest()
-    servers = s.get_servers()
-    opt = get_lower_key(servers)
-    s.servers = {}
-    s.servers[opt] = servers[opt]
+    if(t == 1):
+        servers = s.get_servers()
+        opt = get_lower_key(servers)
+        s.servers = {}
+        s.servers[opt] = servers[opt]
     s.download() #b/s
     s.upload() #b/s
     return s.results
 
 def main(output_file):
     
-    time.sleep(random.randrange(90)) #wait random time betwenn 10s
+    #time.sleep(random.randrange(90)) #wait random time betwenn 10s
 
     # Open type : happend if only one execution or create a new file
     if(os.path.isfile(output_file)):
@@ -48,7 +49,7 @@ def main(output_file):
         
         now = datetime.now()
         try:
-            res = bw_speedtest()
+            res = bw_speedtest(0)
             writer.writerow([now,res.server["country"],res.server["name"],res.server["host"],res.server["id"], res.download, res.upload, res.ping, res.server["d"]])
         except:
              writer.writerow([now,'NaN'])
